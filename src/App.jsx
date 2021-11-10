1bf57f8d0bdf13736718de45dd2125972a2cac76
Jovica Veljanovski <Jovica.Veljanovski@dnb.no>
2021-11-10 16:19:29 +0100

    initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,39 +8,16 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <Bloomberg lines="5" />
-        <Button text="Hello from eufemia" />
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+      <header className="header">
+        FRYD Dashboard
       </header>
+      <div className="main">
+        Boxes Area
+        {[1,2,3,4,5,6].map(x => {
+          return <div className="box">Box {x}</div>
+        })}
+      
+      </div>
     </div>
   )
 }
