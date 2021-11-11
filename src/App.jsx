3f8da1764459f98ceb6f6d0da32086ab10801c36
Eivind Skrede <eivind.lillestol.skrede@dnb.no>
2021-11-11 10:01:25 +0100

    Add settings checkboxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.scss";
 import ComponentFrame from "./components/componentFrame/ComponentFrame";
 import Header from "./components/header/Header/Header";
-import {Heading, Section} from "@dnb/eufemia/components";
-import { DashboardSettings } from './components/DashboardSettings/DashboardSettings'
+import { Heading, Section } from "@dnb/eufemia/components";
+import { DashboardSettings } from "./components/DashboardSettings/DashboardSettings";
 import Bloomberg from "./components/Bloomberg";
 import Ocean from "./components/Ocean/Ocean";
 
@@ -38,28 +38,43 @@ function App() {
       isSelected: true,
     },
   ];
+
+  const settings = [
+    {
+      componentName: "Setting 1",
+      isSelected: true,
+    },
+    {
+      componentName: "Setting 2",
+      isSelected: false,
+    },
+    {
+      componentName: "Setting 3",
+      isSelected: true,
+    },
+  ];
+
   return (
     <div className="App dnb-space dnb-core-style">
       <Header />
       <div className="main">
-      <Heading top="2" bottom="0">FRYD Dashboard</Heading>
-      <div class="settings-container">
-      <DashboardSettings
-        components={[]}
-        onSubmit={() => {}}
-      />
-      </div>
-      
-      <Section style_type="lavender">
-        <div className="box-container">
-          <ComponentFrame
-            componentList={realList
-              .filter((c) => c.isSelected)
-              .map((c) => c.component)}
-          />
+        <Heading top="2" bottom="0">
+          FRYD Dashboard
+        </Heading>
+        <div className="settings-container">
+          <DashboardSettings components={settings} onSubmit={() => {}} />
         </div>
-      </Section>
-    </div>
+
+        <Section style_type="lavender">
+          <div className="box-container">
+            <ComponentFrame
+              componentList={realList
+                .filter((c) => c.isSelected)
+                .map((c) => c.component)}
+            />
+          </div>
+        </Section>
+      </div>
     </div>
   );
 }
