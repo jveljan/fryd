a6fd9484f34f0d944de2f2f861fb8d35e28a92dc
Malin Øien <malin.oien@gmail.com>
2021-11-11 13:10:38 +0100

    Added alerts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import ComponentFrame from "./components/componentFrame/ComponentFrame";
 import Header from "./components/header/Header/Header";
-import { Heading, Section } from "@dnb/eufemia/components";
+import { Heading, Section, Modal } from "@dnb/eufemia/components";
+import { Ul } from "@dnb/eufemia";
 import { DashboardSettings } from "./components/DashboardSettings/DashboardSettings";
 import { useState } from "react";
 import widges from "./widgets";
+import {bell, cog} from "@dnb/eufemia/icons";
 
 function App() {
   const widgetToComponentStructure = (x) => ({
@@ -31,6 +33,24 @@ function App() {
               FRYD Dashboard
             </Heading>
             <div className="settings-container">
+                <Modal
+                    title={"Alerts"}
+                    trigger_text={"Alerts"}
+                    trigger_icon={bell}
+                    trigger_variant="signal"
+                    trigger_icon_position={"left"}
+                    close_button_attributes={{ text: "Close" }}
+                    mode="drawer"
+                    right="1"
+                >
+                    <div>
+                        <Ul>
+                            <li>Item 1</li>
+                            <li>Item 2</li>
+                            <li>Item 3</li>
+                        </Ul>
+                    </div>
+                </Modal>
               <DashboardSettings
                 components={settings}
                 onSubmit={(items) => {
