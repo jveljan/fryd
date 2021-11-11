e59e55c49c10bcb5d9a9030aa069e31b84fba3df
Eivind Skrede <eivind.lillestol.skrede@dnb.no>
2021-11-11 12:26:30 +0100

    Replace checkboxes with togglebuttons

--- src/components/DashboardSettings/DashboardSettings.jsx
+++ src/components/DashboardSettings/DashboardSettings.jsx
@@ -1,8 +1,8 @@
-import { Modal, Checkbox, Button } from "@dnb/eufemia/components";
+import { Modal, ToggleButton, Button } from "@dnb/eufemia/components";
 import { cog } from "@dnb/eufemia/icons";
 
 export const DashboardSettings = (props) => {
-  const items = props.components
+  const items = props.components;
   return (
     <Modal
       title={"Settings"}
@@ -14,20 +14,27 @@ export const DashboardSettings = (props) => {
     >
       {({ close }) => (
         <div>
-          {items.map((item) => {
-            return (
-              <div key={item.componentName}>
-                <Checkbox
-                  label={item.componentName}
-                  label_position="right"
-                  checked={item.isSelected}
-                  on_change={({ checked }) => item.isSelected = checked}
-                  bottom="1"
-                />
-              </div>
-            );
-          })}
-
+          <ToggleButton.Group
+            multiselect="true"
+            values={items.map((item) => item.componentName)}
+            on_change={({ values }) => {
+              console.log("on_change", values);
+            }}
+          >
+            {items.map((item) => {
+              return (
+                <div key={item.componentName}>
+                  <ToggleButton
+                    text={item.componentName}
+                    value={item.componentName}
+                    bottom="1"
+                    on_change={({ checked }) => (item.isSelected = checked)}
+                  />
+                </div>
+              );
+            })}
+          </ToggleButton.Group>
+          <br />
           <Button
             text="Save settings"
             on_click={() => {
