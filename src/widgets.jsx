d9e567a3c7e8de7d010bb0c2f79b7fde8900abdd
Eivind Skrede <eivind.lillestol.skrede@dnb.no>
2021-11-11 11:08:19 +0100

    Add height and width props to PictureComponent

--- src/widgets.jsx
+++ src/widgets.jsx
@@ -1,33 +1,33 @@
 import PictureComponent from "./components/pictureComponent/PictureComponent";
-import picture1 from './assets/picture1.png';
+import picture1 from "./assets/picture1.png";
 import Ocean from "./components/Ocean/Ocean";
 
 export default [
-    {
-      componentName: "Credit Manager",
-      component: (
-        <PictureComponent image={picture1} />
-      ),
-      isSelected: true,
-    },
-    {
-      componentName: "Graphs",
-      component: <Ocean />,
-      isSelected: true,
-    },
-    {
-      componentName: "Widget 3",
-      component: <div>Widget 3</div>,
-      isSelected: true,
-    },
-    {
-      componentName: "Widget 4",
-      component: <div>Widget 4</div>,
-      isSelected: true,
-    },
-    {
-      componentName: "Widget 5",
-      component: <div>Widget 5</div>,
-      isSelected: true,
-    },
-  ];
+  {
+    componentName: "Credit Manager",
+    component: (
+      <PictureComponent image={picture1} height="280px" width="430px" />
+    ),
+    isSelected: true,
+  },
+  {
+    componentName: "Graphs",
+    component: <Ocean />,
+    isSelected: true,
+  },
+  {
+    componentName: "Widget 3",
+    component: <div>Widget 3</div>,
+    isSelected: true,
+  },
+  {
+    componentName: "Widget 4",
+    component: <div>Widget 4</div>,
+    isSelected: true,
+  },
+  {
+    componentName: "Widget 5",
+    component: <div>Widget 5</div>,
+    isSelected: true,
+  },
+];
