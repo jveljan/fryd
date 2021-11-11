83f6cc5dba913c99936a6cf481248d9118204518
Eivind Skrede <eivind.lillestol.skrede@dnb.no>
2021-11-11 11:24:02 +0100

    Fix merge conflict

--- src/widgets.jsx
+++ src/widgets.jsx
@@ -1,57 +1,22 @@
 import PictureComponent from "./components/pictureComponent/PictureComponent";
-<<<<<<< HEAD
-import picture1 from './assets/picture1.png';
+import picture1 from "./assets/picture1.png";
 import IncomeOnInterestRateBarChart from "./components/Graphs/IncomeOnInterestRateBarChart";
 import SomethingLineChart from "./components/Graphs/SomethingLineChart";
 
 export default [
-    {
-      componentName: "Credit Manager",
-      component: (
-        <PictureComponent image={picture1} />
-      ),
-      isSelected: true,
-    },
-    {
-      componentName: "Income On Interest Rate",
-      component: <IncomeOnInterestRateBarChart />,
-      isSelected: true,
-    },
-    {
-      componentName: "Something Line Chart",
-      component: <SomethingLineChart/>,
-      isSelected: true,
-    },
-    {
-      componentName: "Widget 4",
-      component: <div>Widget 4</div>,
-      isSelected: true,
-    },
-    {
-      componentName: "Widget 5",
-      component: <div>Widget 5</div>,
-      isSelected: true,
-    },
-  ];
-=======
-import picture1 from "./assets/picture1.png";
-
-export default [
   {
     componentName: "Credit Manager",
-    component: (
-      <PictureComponent image={picture1} height="280px" width="430px" />
-    ),
+    component: <PictureComponent image={picture1} />,
     isSelected: true,
   },
   {
-    componentName: "Widget 2",
-    component: <div>Widget 2</div>,
+    componentName: "Income On Interest Rate",
+    component: <IncomeOnInterestRateBarChart />,
     isSelected: true,
   },
   {
-    componentName: "Widget 3",
-    component: <div>Widget 3</div>,
+    componentName: "Something Line Chart",
+    component: <SomethingLineChart />,
     isSelected: true,
   },
   {
@@ -65,4 +30,3 @@ export default [
     isSelected: true,
   },
 ];
->>>>>>> 50405fe91f72d55a5ffa54480a0c20701322114c
